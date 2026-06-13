ab18f3eb197fcfdf742bab3ea33fa5a87fa9c0dc
genieDebugs777 <geniuskekana95@gmail.com>
2026-06-13 21:02:37 +0200

    Update seller_orders.php

--- kasi2kasi-connect/seller_orders.php
+++ kasi2kasi-connect/seller_orders.php
@@ -1,6 +1,6 @@
 <?php
-require_once "includes/db.php";
-require_once "includes/auth.php";
+require_once "INCLUDES/db.php";
+require_once "INCLUDES/auth.php";
 requireLogin();
 
 $user_id = $_SESSION["user_id"];
@@ -533,4 +533,4 @@ document.getElementById('selectAll')?.addEventListener('change', function(e) {
 });
 </script>
 
-<?php include "includes/footer.php"; ?>
+<?php include "INCLUDES/footer.php"; ?>

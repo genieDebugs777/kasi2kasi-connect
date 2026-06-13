c7a9bc32babb3b42129f6a044b3cfbf26d023a72
genieDebugs777 <geniuskekana95@gmail.com>
2026-06-13 21:00:03 +0200

    Update profile.php

--- kasi2kasi-connect/profile.php
+++ kasi2kasi-connect/profile.php
@@ -1,6 +1,6 @@
 <?php
-require_once "includes/db.php";
-require_once "includes/auth.php";
+require_once "INCLUDES/db.php";
+require_once "INCLUDES/auth.php";
 requireLogin();
 
 $user_id = $_SESSION["user_id"];
@@ -522,4 +522,4 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 </script>
 
-<?php include "includes/footer.php"; ?>
+<?php include "INCLUDES/footer.php"; ?>

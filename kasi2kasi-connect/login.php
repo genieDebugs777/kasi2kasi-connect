bb8a073455528ce2b7fa61ed2f42f4c56243e3b0
genieDebugs777 <geniuskekana95@gmail.com>
2026-06-13 20:55:28 +0200

    Update login.php

--- kasi2kasi-connect/login.php
+++ kasi2kasi-connect/login.php
@@ -1,6 +1,6 @@
 <?php
-require_once "includes/db.php";
-require_once "includes/auth.php";
+require_once "INCLUDES/db.php";
+require_once "INCLUDES/auth.php";
 
 $error = "";
 
@@ -104,4 +104,4 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
   </div>
 </div>
 
-<?php include "includes/footer.php"; ?>
+<?php include "INCLUDES/footer.php"; ?>
